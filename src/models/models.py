993c8691203be5733e6150f294bdde89600b2fe7
mblgh6 <mblgh6@umsystem.edu>
2024-10-21 13:08:00 -0500

    Adds an MNIST testing with the bench server. Creates a new cooperative model for remote

--- src/models/models.py
+++ src/models/models.py
@@ -120,8 +120,277 @@ class Classifier(pl.LightningModule):
 #-----------------------------------------
 # Initialize: Cooperative Optical Model
 #-----------------------------------------
-
 class CooperativeOpticalModel(pl.LightningModule):
+    def __init__(self, params):
+        super().__init__()
+        self.params = params
+        self.paths = params['paths']
+        self.classifier_image = params['classifier_image']
+        self.alpha, self.beta, self.gamma, self.delta = params['alpha'], params['beta'], params['gamma'], params['delta']
+        self.slm0 = holoeye_pluto21.HoloeyePluto(host=params['slm0_host'])
+        self.slm1 = holoeye_pluto21.HoloeyePluto(host=params['slm1_host'])
+        self.camera_exposure_time = int(params['exposure_time'])
+        self.camera = thorlabs_cc215mu.Thorlabs_CC215MU(exposure_time_us=self.camera_exposure_time)
+        self.dom = DOM(params)
+        self.scaled_plane = self.dom.layers[0].input_plane.scale(0.53, inplace=False)
+        self.classifier = Classifier(params).double()
+        self.register_buffer('background_image', self.get_background_image())
+    #-----------------------------------------
+    # Initialize: Optimizer
+    #-----------------------------------------
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.params['learning_rate'])
+
+    #-----------------------------------------
+    # Initialize: SLM utilities
+    #-----------------------------------------
+    def send_to_slm(self, slm_pattern, which = None):
+        img = Image.fromarray(slm_pattern)
+        if which == 0:
+            img.save('temp0.png')
+            logger.info("Sending image to SLM0")
+            self.slm0.send_scp('temp0.png')
+        elif which == 1:
+            img.save('temp1.png')
+            logger.info("Sending image to SLM1")
+            self.slm1.send_scp('temp1.png')
+        else:
+            raise ValueError(f"SLM {which} not supported")
+
+    def update_slm(self, which = None, wait=None):
+        if which == 0:
+            logger.info("Updating SLM0")
+            self.slm0.update(filename='/root/temp0.png', options=['wl=520', '-fx', '-q'], wait=wait)
+            os.remove('temp0.png')
+        elif which == 1:
+            logger.info("Updating SLM1")
+            self.slm1.update(filename='/root/temp1.png', options=['wl=520', '-fx', '-q'], wait=wait)
+            os.remove('temp1.png')
+        else:
+            raise ValueError(f"SLM {which} not supported")
+
+    def upload_benign_image(self, which=None):
+        logger.warning("Uploading benign image to SLM")
+        image = np.zeros((1080, 1920), dtype=np.uint8)
+        img = Image.fromarray(image)
+        if which == 0:
+            img.save('benign0.png')
+            self.slm0.send_scp('benign0.png')
+            self.slm0.update(filename='/root/benign0.png', options=['wl=520', '-fx', '-q'])
+        elif which == 1:
+            img.save('benign1.png')
+            self.slm1.send_scp('benign1.png')
+            self.slm1.update(filename='/root/benign1.png', options=['wl=520', '-fx', '-q'])
+        else:
+            raise ValueError(f"SLM {which} not supported")
+
+    #-----------------------------------------
+    # Initialize: Camera utilities
+    #-----------------------------------------
+
+    def get_background_image(self):
+        self.upload_benign_image(which=0)
+        return self.get_bench_image().double()
+
+    def get_bench_image(self):
+        # Get the image from the camera
+        image = None
+        self.camera.camera.issue_software_trigger()
+        while image is None:
+            image = self.camera.get_image(pil_image = False, eight_bit = False)
+        image = torch.from_numpy(image).to(self.device)
+
+        return image
+
+    #-----------------------------------------
+    # Initialize: Optical model utilities
+    #-----------------------------------------
+
+    def get_lens_from_dom(self):
+        # Get the phases from the simualtion modulator
+        phases = self.dom.layers[1].modulator.get_phase(with_grad = False)
+
+        # Phase wrap - [0, 2pi]
+        phases = phases % (2 * torch.pi)
+
+        # Convert to [0, 1]
+        phases = phases / (2 * torch.pi)
+
+        # Invert
+        phases = torch.abs(1-phases)
+
+        # Scale to [0, 255]
+        phases = phases * 255
+
+        # Convert to numpy uint8
+        lens_phase = phases.cpu().numpy().squeeze().astype(np.uint8)
+
+        return lens_phase
+
+    #-----------------------------------------
+    # Initialize: Objective function
+    #-----------------------------------------
+    
+    def objective(self, outputs, batch):
+        # Parse the outputs
+        simulation_outputs = outputs['simulation_outputs']
+        simulation_images = simulation_outputs['images']
+        bench_image = outputs['bench_image']
+        classifier_output = outputs['classifier_output']
+        classifier_target = outputs['classifier_target']
+
+        # Parse the batch
+        sample, slm_sample, target = batch
+
+        # We are going to calculate all of the potential losses and select which to use
+        # for the training using MCL parameters
+
+        #-----------------------------------------
+        # Image comparisons
+        #-----------------------------------------
+        # Need to resample the sample to the simulation output plane for comparison
+        sample = spatial_resample(self.scaled_plane, sample.abs(), self.dom.layers[1].output_plane).squeeze()
+        sim_to_ideal = torch.nn.functional.mse_loss(simulation_images, sample)
+        sim_to_bench = torch.nn.functional.mse_loss(simulation_images, bench_image)
+
+        # Bench to ideal requires copying the simulation image tensor to avoid messing up the original
+        # and to allow for backpropagation using the simulation gradient
+        simulation_image_copy = simulation_images.clone()
+        simulation_image_copy.data = bench_image
+        bench_to_ideal = torch.nn.functional.mse_loss(simulation_image_copy, sample)
+
+        #-----------------------------------------
+        # Classifier loss
+        #-----------------------------------------
+        classifier_loss = self.classifier.objective(classifier_output, classifier_target)
+
+
+        # Total loss - right now a soft constrained MCL
+        loss = self.alpha * sim_to_ideal + self.beta * sim_to_bench + self.gamma * bench_to_ideal + self.delta * classifier_loss
+
+        # Return the loss and all the components for logging
+        return loss, sim_to_ideal, sim_to_bench, bench_to_ideal, classifier_loss
+
+    #-----------------------------------------
+    # Initialize: Forward passes
+    #-----------------------------------------
+
+    def dom_forward(self, u:torch.Tensor):
+        simulation_wavefront = self.dom.forward(u)
+        simulation_outputs = self.dom.calculate_auxiliary_outputs(simulation_wavefront)
+        return simulation_outputs
+
+    def bench_forward(self, slm_sample):
+        lens_phase = self.get_lens_from_dom()
+        # Send to SLM
+        slm_sample = slm_sample.squeeze().cpu().numpy().astype(np.uint8)
+        self.send_to_slm(slm_sample, which=0)
+        self.update_slm(which=0)
+        self.send_to_slm(lens_phase, which=1)
+        self.update_slm(which=1, wait=0.5)
+        # Get the image from the camera
+        image = self.get_bench_image()
+        image = torch.abs(image - self.background_image)
+        return image, lens_phase
+
+    def classifier_forward(self, sample, target):
+        # reshape the sample into [b,1,h,w]
+        if len(sample.shape) == 2:
+            sample = sample.unsqueeze(0).unsqueeze(0)
+        elif len(sample.shape) == 3:
+            sample = sample.unsqueeze(1)
+        batch = (sample, target)
+        prediction, target = self.classifier.shared_step(batch)
+        return prediction, target
+
+    def forward(self, samples, slm_samples, classifier_targets):
+        # Run the simulation forward
+        simulation_outputs = self.dom_forward(samples)
+
+        # Run the benchtop forward
+        bench_image, phases = self.bench_forward(slm_samples)
+
+        # Run the classifier forward
+        if self.classifier_image == 'bench':
+            classifier_image = bench_image
+        elif self.classifier_image == 'sim':
+            classifier_image = simulation_outputs['images']
+        elif self.classifier_image == 'ideal':
+            classifier_image = samples.abs()
+        else:
+            raise ValueError(f"Classifier image {self.classifier_image} not supported")
+
+        classifier_output, classifier_target = self.classifier_forward(classifier_image, classifier_targets)
+
+        return {'simulation_outputs': simulation_outputs,
+                'bench_image': bench_image,
+                'phases': phases,
+                'classifier_output': classifier_output,
+                'classifier_target': classifier_target}
+
+    #-----------------------------------------
+    # Initialize: Training utilities
+    #-----------------------------------------
+
+    def on_train_start(self):
+        self.upload_benign_image(which=0)
+
+    def on_train_epoch_end(self):
+        # Get the phases from the simulation modulator
+        phases = self.get_lens_from_dom()
+        # Get the current epoch
+        epoch = self.current_epoch
+        # Save the lens phase
+        torch.save(phases, os.path.join(self.paths['path_lens_phase'], f'lens_phase_{epoch:04d}.pt'))
+
+    def shared_step(self, batch):
+        samples, slm_samples, classifier_targets = batch
+
+        # Run the forward pass
+        outputs = self(samples, slm_samples, classifier_targets)
+
+        # Parse the outputs
+        simulation_outputs = outputs['simulation_outputs']
+        bench_image = outputs['bench_image']
+        phases = outputs['phases']
+        classifier_output = outputs['classifier_output']
+        classifier_target = outputs['classifier_target']
+
+        return {'simulation_outputs': simulation_outputs,
+                'bench_image': bench_image,
+                'phases': phases,
+                'classifier_output': classifier_output,
+                'classifier_target': classifier_target,
+                }
+
+    def training_step(self, batch, batch_idx):
+        outputs = self.shared_step(batch)
+        # Get the loss
+        loss, sim_to_ideal, sim_to_bench, bench_to_ideal, classifier_loss = self.objective(outputs, batch)
+        # Log the loss
+        self.log('loss_train', loss, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('sim_to_ideal_train', sim_to_ideal, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('sim_to_bench_train', sim_to_bench, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('bench_to_ideal_train', bench_to_ideal, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('classifier_loss_train', classifier_loss, on_step=False, on_epoch=True, sync_dist=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        outputs = self.shared_step(batch)
+        # Get the loss
+        loss, sim_to_ideal, sim_to_bench, bench_to_ideal, classifier_loss = self.objective(outputs, batch)
+        # Log the loss
+        self.log('loss_val', loss, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('sim_to_ideal_val', sim_to_ideal, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('sim_to_bench_val', sim_to_bench, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('bench_to_ideal_val', bench_to_ideal, on_step=False, on_epoch=True, sync_dist=True)
+        self.log('classifier_loss_val', classifier_loss, on_step=False, on_epoch=True, sync_dist=True)
+        return loss
+
+#-----------------------------------------
+# Initialize: Cooperative Optical Model (remote)
+#-----------------------------------------
+class CooperativeOpticalModelRemote(pl.LightningModule):
     def __init__(self, params):
         super().__init__()
         self.params = params
--- src/utils/mnist_testing_with_flask.py
+++ src/utils/mnist_testing_with_flask.py
@@ -1,1 +1,201 @@
 #
+import os
+import yaml
+import argparse
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+import torch
+
+import requests
+import sys
+sys.path.append('../')
+sys.path.append('../../')
+from src.datamodule.datamodule import select_data
+from diffractive_optical_model.diffractive_optical_model import DOM
+
+def add_slm(slm_name, slm_host, bench_server_url, add_slm_endpoint):
+    # Add an SLM
+    slm_params = {  'slm_name': slm_name,
+                    'slm_host': slm_host,
+                  }
+    response = requests.post(bench_server_url + add_slm_endpoint, json=slm_params)
+    return response
+
+def add_camera(camera_name, camera_exposure_time, bench_server_url, add_camera_endpoint):
+    # Add a camera
+    camera_params = {   'camera_name': camera_name,
+                        'camera_exposure_time': camera_exposure_time,
+                    }
+    response = requests.post(bench_server_url + add_camera_endpoint, json=camera_params)
+    return response
+
+def get_camera_image(camera_name, bench_server_url, get_camera_image_endpoint):
+    # Get an image from the camera
+    payload = {'camera_name': camera_name}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(bench_server_url + get_camera_image_endpoint, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data['status'] == 'ok':
+            image = base64.b64decode(data['image'])
+            image = BytesIO(image)
+            image = np.load(image)
+            return image
+        else:
+            print(f"Error : {data['message']}")
+            return None
+    else:
+        print(f"Error : {response.status_code} - {response.reason}")
+        return None
+
+def send_to_slm(slm_name, options, wait, bench_server_url, update_slm_endpoint, image):
+    # If the image is a numpy array, convert it to a PIL image
+    if isinstance(image, np.ndarray):
+        image = Image.fromarray(image)
+
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    buffered.seek(0)
+
+    files = {'image': ('image.png', buffered, 'image/png')}
+    payload = {'slm_name': slm_name, 'options': options, 'wait': wait}
+    response = requests.post(bench_server_url + update_slm_endpoint, files=files, data=payload)
+    return response
+
+
+def get_bench_info(bench_server_url):
+    # Get the bench info
+    info_api_endpoint = '/info'
+    response = requests.get(bench_server_url + info_api_endpoint)
+    return response
+
+def get_lens_from_dom(dom):
+    # Get the phases from the simualtion modulator
+    phases = dom.layers[1].modulator.get_phase(with_grad = False)
+
+    # Phase wrap - [0, 2pi]
+    phases = phases % (2 * torch.pi)
+
+    # Convert to [0, 1]
+    phases = phases / (2 * torch.pi)
+
+    # Invert
+    phases = torch.abs(1-phases)
+
+    # Scale to [0, 255]
+    phases = phases * 255
+
+    # Convert to numpy uint8
+    lens_phase = phases.cpu().numpy().squeeze().astype(np.uint8)
+
+    return lens_phase
+
+if __name__ == "__main__":
+
+    import sys
+    sys.path.append('../')
+    import time
+
+    # Read the CLA
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--wl', type=float, default=0, help='Wavelength of the light')
+    parser.add_argument("--delay", type=float, default=0.5, help="Delay between updates")
+    args = parser.parse_args()
+
+    # Load the config file
+    config = yaml.load(open("../../config.yaml", "r"), Loader=yaml.FullLoader)
+    config['paths']['path_root'] = '../../'
+
+    slm0_host = config['slm0_host']
+    slm1_host = config['slm1_host']
+    camera_exposure_time = config['exposure_time']
+
+    # Bench server URL and endpoints
+    bench_server_url = config['bench_server_url']
+    add_slm_endpoint = config['add_slm_endpoint']
+    remove_slm_endpoint = config['remove_slm_endpoint']
+    add_camera_endpoint = config['add_camera_endpoint']
+    remove_camera_endpoint = config['remove_camera_endpoint']
+    update_slm_endpoint = config['update_slm_endpoint']
+    get_camera_image_endpoint = config['get_camera_image_endpoint']
+    reset_bench_endpoint = config['reset_bench_endpoint']
+
+    # Load the data module
+    datamodule = select_data(config)
+    datamodule.setup()
+
+    # Reset the bench
+    response = requests.post(bench_server_url + reset_bench_endpoint)
+
+    train_loader = datamodule.train_dataloader()
+
+    # Create the slm options
+    options = ['shf=1', 'wl=520','-fx', '-q']
+
+    # Create SLMs
+    slm0_name = 'slm0'
+    slm1_name = 'slm1'
+    response = add_slm(slm0_name, slm0_host, bench_server_url, add_slm_endpoint)
+    response = add_slm(slm1_name, slm1_host, bench_server_url, add_slm_endpoint)
+    # Create camera
+    camera_name = 'thorlabs_cc215mu'
+    response = add_camera(camera_name, camera_exposure_time, bench_server_url, add_camera_endpoint)
+    print(get_bench_info(bench_server_url).text)
+
+    # Initialize a DOM simulation
+    dom = DOM(config)
+
+    # Get the lens phases from the DOM
+    lens_phase = get_lens_from_dom(dom)
+
+    # Send the lens phase to slm1
+    wait = 2
+    response = send_to_slm(slm1_name, options, wait, bench_server_url, update_slm_endpoint, lens_phase)
+
+    # Get the intial plane params from the config file
+    plane_params = config['planes'][0]
+
+    # Get the spatial extent of the plane
+    lx, ly = plane_params['size']
+    nx, ny = plane_params['Nx'], plane_params['Ny']
+
+    # Generate an intial image to send to the SLM
+    image = np.zeros((nx, ny), dtype=np.uint8)
+
+    # Send the image to the SLM0
+    response = send_to_slm(slm0_name, options, wait, bench_server_url, update_slm_endpoint, image)
+
+    # Get a background image
+    image = get_camera_image(camera_name, bench_server_url, get_camera_image_endpoint)
+    background_image = np.asarray(image)
+
+    fig, ax = plt.subplots(3,1)
+    im0 = ax[0].imshow(background_image, cmap='gray', vmin=0, vmax=1)
+    im1 = ax[1].imshow(background_image, cmap='gray', vmin=0, vmax=1)
+    im2 = ax[2].imshow(background_image, cmap='gray', vmin=0, vmax=1)
+
+    counter = 0
+    while(1):
+        batch = next(iter(train_loader))
+        images, slm_sample, labels = batch
+        # Send to slm
+        response = send_to_slm(slm0_name, options, 0.5, bench_server_url, update_slm_endpoint, slm_sample.squeeze().numpy())
+        time.sleep(args.delay)
+
+        # Get camera image
+        image = get_camera_image(camera_name, bench_server_url, get_camera_image_endpoint)
+        camera_image = image - background_image
+        camera_image = camera_image / np.max(camera_image)
+        im0.set_data(camera_image)
+        im1.set_data(images.squeeze().abs().numpy())
+
+        difference = np.abs(camera_image - images.squeeze().abs().numpy())
+        difference = difference / np.max(difference)
+        im2.set_data(difference)
+        fig.suptitle('Frame {}'.format(counter))
+        plt.pause(args.delay)
+        counter += 1
