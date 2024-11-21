ed7deb60eded66213275b40cd517e7139214cdc2
mblgh6 <mblgh6@umsystem.edu>
2024-11-21 17:59:26 -0600

    adds the notebooks

--- src/evaluation/eval_sim2real.py
+++ src/evaluation/eval_sim2real.py
@@ -118,7 +118,7 @@ def plot_differences(model, data_files):
         plt.pause(1)
 
 
-def get_calibration_layer(model):
+def get_calibration_layer(model, save=False, path_save=None):
 
     # Get the calibration layer
     #amplitude = model.dom.layers[1].modulator.get_amplitude().squeeze().cpu().detach().numpy()
@@ -126,6 +126,12 @@ def get_calibration_layer(model):
     amplitude = model.dom.layers[1].modulator.optimizeable_amplitude.squeeze().cpu().detach().numpy()
     phase = model.dom.layers[1].modulator.optimizeable_phase.squeeze().cpu().detach().numpy()
     calibration_layer = amplitude * np.exp(1j*phase)
+
+    if save:
+        torch.save(amplitude, os.path.join(path_save, 'calibration_amplitude.pt'))
+        torch.save(phase, os.path.join(path_save, 'calibration_phase.pt'))
+        torch.save(calibration_layer, os.path.join(path_save, 'calibration_layer.pt'))
+
     return calibration_layer
 
 
@@ -145,6 +151,33 @@ def plot_calibration_layer(layer):
     ax[1].axis('off')
 
     plt.show()
+
+
+def run_model(model, data_files, save=False, path_save=None):
+
+    for i,file in enumerate(tqdm(data_files)):
+        # Load the data
+        data = torch.load(file, weights_only=True)
+        sample = data['resampled_sample']
+        bench_image = data['bench_image'].squeeze().cpu().detach()
+        original_sim_output = data['sim_output'].abs().detach().cpu()**2
+
+        if len(sample.shape) == 2:
+            sample = sample.unsqueeze(0).unsqueeze(0)
+        elif len(sample.shape) == 3:
+            sample = sample.unsqueeze(0)
+
+        # Forward pass
+        with torch.no_grad():
+            sim_output = model.forward(sample.cpu()).detach().abs().cpu()**2
+
+        if save:
+            split = file.split('/')[-1]
+            split = split.split('_')
+            split[0] = 'sim2real'
+            split = '_'.join(split)
+            data = {'resampled_sample': sample, 'bench_image': bench_image, 'sim2real_output': sim_output, 'sim_output': original_sim_output}
+            torch.save(data, os.path.join(path_save, split))
 
 if __name__ == "__main__":
 
@@ -169,13 +202,15 @@ if __name__ == "__main__":
     valid_files = [os.path.join(path_data, f) for f in files if 'valid' in f]
 
     # Plot the calibration layer
-    calibration_layer = get_calibration_layer(model)
-    from IPython import embed; embed()
-    plot_calibration_layer(calibration_layer)
+    #calibration_layer = get_calibration_layer(model, save=True, path_save=path_experiment)
+    #plot_calibration_layer(calibration_layer)
 
     # Plot the images
-    plot_images(model, train_files+valid_files)
+    #plot_images(model, train_files+valid_files)
     #plot_differences(model, train_files+valid_files)
+
+    # Save the new simulated images
+    run_model(model, train_files+valid_files, save=True, path_save = '../../data/sim2real/')
 
 
 
