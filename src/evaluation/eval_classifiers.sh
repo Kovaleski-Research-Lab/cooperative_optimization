9c52768472b35a396b06d7a98071e5c5a7d7467f
Marshall Lindsay <mblgh6@umsystem.edu>
2024-11-25 12:50:23 -0600

    Adds more resources to the evaluation job. Fixes script calling

--- src/evaluation/eval_classifiers.sh
+++ src/evaluation/eval_classifiers.sh
@@ -4,15 +4,15 @@
 # Evaluate the classifiers
 #checkpoint_path = '/devleop/results/classifier_baseline_bench_resampled_sample/version_1/'
 
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_resampled_sample/version_2/'
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_resampled_sample/version_3/'
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_resampled_sample/version_4/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_resampled_sample/version_2/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_resampled_sample/version_3/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_resampled_sample/version_4/'
 
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_sim_output/version_2/'
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_sim_output/version_3/'
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_sim_output/version_4/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_sim_output/version_2/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_sim_output/version_3/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_sim_output/version_4/'
 
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_bench_image/version_2/'
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_bench_image/version_3/'
-python evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_bench_image/version_4/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_bench_image/version_2/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_bench_image/version_3/'
+python /develop/code/cooperative_optimization/src/evaluation/evaluation.py --checkpoint_path '/develop/results/classifier_baseline_bench_bench_image/version_4/'
 
