b6b35be31d051bbd630ce8eba4ecd6d7e1754f53
Marshall Lindsay <mblgh6@umsystem.edu>
2025-01-08 17:13:15 -0600

    Updates sim2real for crop with mse

--- src/models/models.py
+++ src/models/models.py
@@ -781,9 +781,9 @@ class Sim2Real(pl.LightningModule):
         bench_image = batch[1]
         sim_image = self.crop(sim_image)
         bench_image = self.crop(bench_image)
-        #loss = torch.nn.functional.mse_loss(sim_image.squeeze(), bench_image.squeeze())
-        loss = psnr(sim_image.squeeze(), bench_image.squeeze())
-        loss = 1 / loss
+        loss = torch.nn.functional.mse_loss(sim_image.squeeze(), bench_image.squeeze())
+        #loss = psnr(sim_image.squeeze(), bench_image.squeeze())
+        #loss = 1 / loss
         return loss
 
     def forward(self, u:torch.Tensor):
